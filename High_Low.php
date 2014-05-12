f633ec5300552e0b2c0e4487427a39c4b0cc2e1d
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-12 11:48:06 -0500

    High Low game with User greeting, guessing, and winning

--- High_Low.php
+++ High_Low.php
@@ -5,6 +5,9 @@
 //User will continue to guess the number
 //Computer will outputs higher or lower
 
+$Winning_Number = rand(1, 100);
+$Number_of_Guesses = 1;
+
 fwrite(STDOUT, 'What is your first name? ');
 
 // Get the input from user
@@ -13,6 +16,25 @@ $first_name = fgets(STDIN);
 // Output the user's name
 fwrite(STDOUT, "Hello $first_name" . "I'm thinking of a number between 1 and 100.\nCan you guess which one it is?\n");
  
+$User_Guess = fgets(STDIN);
 
+while($User_Guess != $Winning_Number)
+{
+	if($User_Guess < $Winning_Number)
+	{
+		echo "Higher";
+	}
+	else 
+	{
+		echo "Lower";
+	}
+fwrite(STDOUT, "Guess again!\n");
+$User_Guess = fgets(STDIN);
+$Number_of_Guesses++;
+}
 
-//int rand ( int $min , int $max )
+if ($User_Guess == $Winning_Number)
+{
+	echo "Winner Winner Chicken Dinner. It took you {$Number_of_Guesses} guesses!" . PHP_EOL
+	;
+}
