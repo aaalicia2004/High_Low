9bd05e21cecb98870e7d1bc9028c2a0765fd9de2
aaalicia2004 <aaalicia2004@yahoo.com>
2014-06-25 13:45:42 -0500

    Update High_Low.php

--- High_Low.php
+++ High_Low.php
@@ -1,7 +1,41 @@
 <?php
 
-var_dump($argv);
 
+$min = $argv[1];
+$max = $argv[2];
+$Winning_Number = rand($min, $max);
+$Number_of_Guesses = 1;
+
+fwrite(STDOUT, 'What is your first name? ');
+
+// Get the input from user
+$first_name = fgets(STDIN);
+
+// Output the user's name
+fwrite(STDOUT, "Hello $first_name" . "I'm thinking of a number between $min and $max.\nCan you guess which one it is?\n");
+ 
+$User_Guess = fgets(STDIN);
+
+while($User_Guess != $Winning_Number)
+{
+	if($User_Guess < $Winning_Number)
+	{
+	    echo "Higher\n";
+	}
+	else 
+	{
+	    echo "Lower\n";
+	}
+    fwrite(STDOUT, "Guess again!\n");
+    $User_Guess = fgets(STDIN);
+    $Number_of_Guesses++;
+}
+
+if ($User_Guess == $Winning_Number)
+{
+    echo "Winner Winner Chicken Dinner. It took you {$Number_of_Guesses} guesses!" . PHP_EOL;
+}
+//Steps to create the game:
 //Get User's first name
 //Computer will generate a number
 //User will continue to guess the number
@@ -24,39 +58,4 @@ var_dump($argv);
 	//if the guess and the number are the same say "winner!"" and end the game"
 
 //End the game
-
-$min = $argv[1];
-$max = $argv[2];
-$Winning_Number = rand($min, $max);
-$Number_of_Guesses = 1;
-
-fwrite(STDOUT, 'What is your first name? ');
-
-// Get the input from user
-$first_name = fgets(STDIN);
-
-// Output the user's name
-fwrite(STDOUT, "Hello $first_name" . "I'm thinking of a number between $min and $max.\nCan you guess which one it is?\n");
- 
-$User_Guess = fgets(STDIN);
-
-while($User_Guess != $Winning_Number)
-{
-	if($User_Guess < $Winning_Number)
-	{
-		echo "Higher\n";
-	}
-	else 
-	{
-		echo "Lower\n";
-	}
-fwrite(STDOUT, "Guess again!\n");
-$User_Guess = fgets(STDIN);
-$Number_of_Guesses++;
-}
-
-if ($User_Guess == $Winning_Number)
-{
-	echo "Winner Winner Chicken Dinner. It took you {$Number_of_Guesses} guesses!" . PHP_EOL
-	;
-}
+?>
